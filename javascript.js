ea3d2feb8641eb1cac08eaead836adbdb7e05b3b
meghangantt <meghangantt@gmail.com>
2022-10-12 15:11:04 -0400

    Create gridbase variable

--- javascript.js
+++ javascript.js
@@ -1,17 +1,22 @@
 const grid = document.querySelector('#grid');
 const square = document.createElement('div');
 square.classList.add('square');
+let gridBase = 16;
+square.style.cssText = `height: ${500/gridBase}px; width: ${500/gridBase}px`;
+
+grid.style.cssText = `grid-template-columns: repeat(${gridBase}, auto)`;
 
 grid.appendChild(square);
 
-for (let i = 1; i < 256; i++) {
+for (let i = 1; i < gridBase**2; i++) {
     grid.appendChild(square.cloneNode(true));
 }
 
 const allSquares = document.querySelectorAll('.square');
 
-allSquares.forEach((one) => {
-    one.addEventListener('mouseover', () => {
-      one.classList.add('hovered');
+allSquares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.classList.add('hovered');
     });
   });
+
