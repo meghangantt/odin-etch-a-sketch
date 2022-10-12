c6e1d7ee0a4b3a122fa20dd5873dee9bce855317
meghangantt <meghangantt@gmail.com>
2022-10-12 14:22:48 -0400

    Create event listener for mouseover

--- javascript.js
+++ javascript.js
@@ -7,3 +7,11 @@ grid.appendChild(square);
 for (let i = 1; i < 256; i++) {
     grid.appendChild(square.cloneNode(true));
 }
+
+const allSquares = document.querySelectorAll('.square');
+
+allSquares.forEach((one) => {
+    one.addEventListener('mouseover', () => {
+      one.classList.add('hovered');
+    });
+  });
