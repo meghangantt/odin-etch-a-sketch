d5da2d6684f291cfc524d641112c1d3875642eb2
meghangantt <meghangantt@gmail.com>
2022-10-12 15:16:32 -0400

    Make createGrid function with previous code

--- javascript.js
+++ javascript.js
@@ -1,22 +1,26 @@
-const grid = document.querySelector('#grid');
-const square = document.createElement('div');
-square.classList.add('square');
 let gridBase = 16;
-square.style.cssText = `height: ${500/gridBase}px; width: ${500/gridBase}px`;
 
-grid.style.cssText = `grid-template-columns: repeat(${gridBase}, auto)`;
+const createGrid = () => {
+    const grid = document.querySelector('#grid');
+    grid.style.cssText = `grid-template-columns: repeat(${gridBase}, auto)`;
 
-grid.appendChild(square);
-
-for (let i = 1; i < gridBase**2; i++) {
-    grid.appendChild(square.cloneNode(true));
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.cssText = `height: ${500/gridBase}px; width: ${500/gridBase}px`;
+    
+    grid.appendChild(square);
+    
+    for (let i = 1; i < gridBase**2; i++) {
+        grid.appendChild(square.cloneNode(true));
+    }
+    
+    const allSquares = document.querySelectorAll('.square');
+    
+    allSquares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hovered');
+        });
+      });
 }
 
-const allSquares = document.querySelectorAll('.square');
-
-allSquares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('hovered');
-    });
-  });
-
+createGrid();
